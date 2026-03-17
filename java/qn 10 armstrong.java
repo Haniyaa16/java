e30cffb58fa02aed86d1db8985c746e3e50f610d
Haniyaa16 <haniyakk1234@gmail.com>
2026-03-17 11:33:10 +0530

    Update qn 10 armstrong.java

--- java/qn 10 armstrong.java
+++ java/qn 10 armstrong.java
@@ -1,40 +1,40 @@
-  1 import java.util.Scanner;
-  2 class Armstrong
-  3 {
-  4     int num, sum = 0, temp;
-  5     void get(int n)
-  6     {
-  7         num = n;
-  8         temp = num;
-  9     }
- 10
- 11     void check()
- 12     {
- 13         while (temp > 0)
- 14         {
- 15             int r = temp % 10;
- 16             sum = sum + (r * r * r);
- 17             temp = temp / 10;
- 18         }
- 19
- 20         if (sum == num)
- 21             System.out.println("Armstrong Number");
- 22         else
- 23             System.out.println("Not Armstrong Number");
- 24     }
- 25 }
- 26
- 27 public class arstrong
- 28 {
- 29     public static void main(String args[])
- 30     {
- 31         Scanner sc = new Scanner(System.in);
- 32
- 33         System.out.print("Enter a number: ");
- 34         int n = sc.nextInt();
- 35
- 36         Armstrong ob = new Armstrong();
- 37         ob.get(n);
- 38         ob.check();
- 39     }
- 40 }
+   import java.util.Scanner;
+   class Armstrong
+   {
+       int num, sum = 0, temp;
+       void get(int n)
+       {
+         num = n;
+           temp = num;
+       }
+ 
+      void check()
+      {
+          while (temp > 0)
+          {
+              int r = temp % 10;
+              sum = sum + (r * r * r);
+              temp = temp / 10;
+          }
+ 
+          if (sum == num)
+              System.out.println("Armstrong Number");
+          else
+              System.out.println("Not Armstrong Number");
+      }
+  }
+ 
+  public class arstrong
+  {
+      public static void main(String args[])
+      {
+          Scanner sc = new Scanner(System.in);
+ 
+          System.out.print("Enter a number: ");
+          int n = sc.nextInt();
+ 
+          Armstrong ob = new Armstrong();
+          ob.get(n);
+         ob.check();
+      }
+  }
