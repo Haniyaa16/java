c2e5ad39c418f06af617b406ffaab4a894b2ffac
Haniyaa16 <haniyakk1234@gmail.com>
2026-03-17 11:41:36 +0530

    Update qn 9 reverse.java

--- java/qn 9 reverse.java
+++ java/qn 9 reverse.java
@@ -1,27 +1,27 @@
-  1 import java.util.Scanner;
-  2
-  3 class Reverse {
-  4     int n, rev = 0;
-  5
-  6     void get() {
-  7         Scanner sc = new Scanner(System.in);
-  8         System.out.print("Enter number: ");
-  9         n = sc.nextInt();
- 10     }
- 11
- 12     void revv() {
- 13         while (n > 0) {
- 14             rev = rev * 10 + n % 10;
- 15             n = n / 10;
- 16         }
- 17         System.out.println("Reversed number = " + rev);
- 18     }
- 19 }
- 20
- 21 class rev {
- 22     public static void main(String[] args) {
- 23         Reverse r = new Reverse();
- 24         r.get();
- 25         r.revv();
- 26     }
- 27 }
+import java.util.Scanner;
+
+class Reverse {
+    int n, rev = 0;
+
+    void get() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        n = sc.nextInt();
+    }
+
+    void revv() {
+        while (n > 0) {
+            rev = rev * 10 + n % 10;
+            n = n / 10;
+        }
+        System.out.println("Reversed number = " + rev);
+    }
+}
+
+class rev {
+    public static void main(String[] args) {
+        Reverse r = new Reverse();
+        r.get();
+        r.revv();
+    }
+}
