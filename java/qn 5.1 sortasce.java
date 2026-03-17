e6c62dfd31de45a1b4aa07a61d9a9808df71eb94
Haniyaa16 <haniyakk1234@gmail.com>
2026-03-17 11:39:45 +0530

    Update qn 5.1 sortasce.java

--- java/qn 5.1 sortasce.java
+++ java/qn 5.1 sortasce.java
@@ -1,35 +1,39 @@
-  1 import java.util.Scanner;
-  2 class qnsort
-  3  {
-  4    public static void main(String args[])
-  5     {
-  6         Scanner sc=new Scanner(System.in);
-  7
-  8        System.out.print("Enter no of elements: ");
-  9          int n=sc.nextInt();
- 10
- 11         int ar[]=new int[n];
- 12         System.out.print("Enter "+n+" elements: ");
- 13         for(int i=0;i<n;i++)
- 14         {
- 15             ar[i]=sc.nextInt();
- 16         }
- 17         for(int i=0;i<n-1;i++)
- 18         {
- 19             for(int j=i+1;j<n;j++)
- 20             {
- 21                 if(ar[i]>ar[j])
- 22                 {
- 23                     int temp=ar[i];
- 24                     ar[i]=ar[j];
- 25                     ar[j]=temp;
- 26                 }
- 27             }
- 28         }
- 29         System.out.print("Ascending order: ");
- 30         for(int i=0;i<n;i++)
- 31         {
- 32             System.out.print(ar[i]+" ");
- 33         }
- 34     }
- 35  }
+ import java.util.Scanner;
+
+class qnsort
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter no of elements: ");
+        int n = sc.nextInt();
+
+        int ar[] = new int[n];
+        System.out.print("Enter " + n + " elements: ");
+
+        for(int i = 0; i < n; i++)
+        {
+            ar[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < n - 1; i++)
+        {
+            for(int j = i + 1; j < n; j++)
+            {
+                if(ar[i] > ar[j])
+                {
+                    int temp = ar[i];
+                    ar[i] = ar[j];
+                    ar[j] = temp;
+                }
+            }
+        }
+
+        System.out.print("Ascending order: ");
+        for(int i = 0; i < n; i++)
+        {
+            System.out.print(ar[i] + " ");
+        }
+    }
+}
