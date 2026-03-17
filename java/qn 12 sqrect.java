7d9d0aa894b874a871266db05961f329f738c1fc
Haniyaa16 <haniyakk1234@gmail.com>
2026-03-17 11:36:10 +0530

    Update qn 12 sqrect.java

--- java/qn 12 sqrect.java
+++ java/qn 12 sqrect.java
@@ -1,39 +1,39 @@
-  1 import java.util.Scanner;
-  2
-  3 class Rectangle
-  4 {
-  5     int side, breadth, len, area, peri;
-  6
-  7     void get()
-  8     {
-  9         Scanner sc = new Scanner(System.in);
- 10         System.out.print("Enter side of square: ");
- 11         side = sc.nextInt();
- 12         System.out.print("Enter breadth of rectangle: ");
- 13         breadth = sc.nextInt();
- 14     }
- 15
- 16     void calculate()
- 17     {
- 18         len = (side * side) / breadth;
- 19         area = len * breadth;
- 20         peri = 2 * (len + breadth);
- 21     }
- 22
- 23     void display() {
- 24         System.out.println("Length of rectangle = " + len);
- 25         System.out.println("Area of rectangle = " + area);
- 26         System.out.println("Perimeter of rectangle = " + peri);
- 27     }
- 28 }
- 29
- 30 public class sqrect
- 31 {
- 32     public static void main(String args[])
- 33     {
- 34         Rectangle r = new Rectangle();
- 35         r.get();
- 36         r.calculate();
- 37         r.display();
- 38     }
- 39 }
+   import java.util.Scanner;
+  
+ class Rectangle
+ {
+     int side, breadth, len, area, peri;
+  
+       void get()
+       {
+           Scanner sc = new Scanner(System.in);
+          System.out.print("Enter side of square: ");
+          side = sc.nextInt();
+          System.out.print("Enter breadth of rectangle: ");
+          breadth = sc.nextInt();
+      }
+ 
+      void calculate()
+      {
+          len = (side * side) / breadth;
+          area = len * breadth;
+          peri = 2 * (len + breadth);
+      }
+ 
+      void display() {
+          System.out.println("Length of rectangle = " + len);
+          System.out.println("Area of rectangle = " + area);
+          System.out.println("Perimeter of rectangle = " + peri);
+      }
+  }
+ 
+  public class sqrect
+  {
+     public static void main(String args[])
+      {
+          Rectangle r = new Rectangle();
+          r.get();
+          r.calculate();
+          r.display();
+      }
+  }
