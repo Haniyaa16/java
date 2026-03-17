3589c6cf06bc7ef86125c5a5b290e2a75a79f04d
Haniyaa16 <haniyakk1234@gmail.com>
2026-03-17 11:31:45 +0530

    Update qn 8 sumdigit.java

--- java/qn 8 sumdigit.java
+++ java/qn 8 sumdigit.java
@@ -1,31 +1,31 @@
-  1 import java.util.Scanner;
-  2
-  3 class Sum
-  4 {
-  5     void findSum(int num)
-  6     {
-  7         int sum = 0;
-  8
-  9         while (num > 0)
- 10         {
- 11             sum = sum + num % 10;
- 12             num = num / 10;
- 13         }
- 14
- 15         System.out.println("Sum of digits = " + sum);
- 16     }
- 17 }
- 18
- 19 public class sumdigit
- 20 {
- 21     public static void main(String args[])
- 22     {
- 23         Scanner sc = new Scanner(System.in);
- 24
- 25         System.out.print("Enter a number: ");
- 26         int n = sc.nextInt();
- 27
- 28         Sum s = new Sum();
- 29         s.findSum(n);
- 30     }
- 31 }
+   import java.util.Scanner;
+  
+   class Sum
+   {
+       void findSum(int num)
+       {
+           int sum = 0;
+  
+           while (num > 0)
+          {
+              sum = sum + num % 10;
+              num = num / 10;
+          }
+ 
+          System.out.println("Sum of digits = " + sum);
+      }
+  }
+ 
+  public class sumdigit
+  {
+      public static void main(String args[])
+      {
+          Scanner sc = new Scanner(System.in);
+ 
+          System.out.print("Enter a number: ");
+          int n = sc.nextInt();
+ 
+          Sum s = new Sum();
+          s.findSum(n);
+      }
+  }
