e8903592edd2b60103c8d132b1b1169b1a370c2b
Haniyaa16 <haniyakk1234@gmail.com>
2026-03-17 11:40:36 +0530

    Update qn 6 prime.java

--- java/qn 6 prime.java
+++ java/qn 6 prime.java
@@ -1,33 +1,40 @@
-  1 import java.util.Scanner;
-  2 public class qnprime
-  3 {
-  4     public static void main(String args[])
-  5     {
-  6         Scanner sc=new Scanner(System.in);
-  7         System.out.println("Enter no. of elements: ");
-  8         int n=sc.nextInt();
-  9         int ar[]=new int[n];
- 10         System.out.println("Enter elements: ");
- 11         for(int i=0;i<n;i++)
- 12         {
- 13             ar[i]=sc.nextInt();
- 14         }
- 15         System.out.println("Prime nos: ");
- 16         for(int i=0;i<n;i++)
- 17         {
- 18             int num=ar[i];
- 19             int count=0;
- 20             for(int j=1;j<=num;j++)
- 21                     {
- 22                         if(num%j==0)
- 23                         {
- 24                             count++;
- 25                         }
- 26                     }
- 27                     if(count==2)
- 28                     {
- 29                         System.out.print(num+" ");
- 30                     }
- 31         }
- 32     }
- 33 }
+ import java.util.Scanner;
+
+public class qnprime
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter no. of elements: ");
+        int n = sc.nextInt();
+
+        int ar[] = new int[n];
+        System.out.println("Enter elements: ");
+
+        for(int i = 0; i < n; i++)
+        {
+            ar[i] = sc.nextInt();
+        }
+
+        System.out.println("Prime nos: ");
+        for(int i = 0; i < n; i++)
+        {
+            int num = ar[i];
+            int count = 0;
+
+            for(int j = 1; j <= num; j++)
+            {
+                if(num % j == 0)
+                {
+                    count++;
+                }
+            }
+
+            if(count == 2)
+            {
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
